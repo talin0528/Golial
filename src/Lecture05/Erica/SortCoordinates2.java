c8de144f99587be9a558671595b0604daaefc885
itleader <itleader@CDT-14-0027.infraware.co.kr>
2017-01-13 13:32:43 +0900

    동일 패키지에 Pair가 있어서 나는 빌드 오류 수정

--- src/Lecture05/Erica/SortCoordinates2.java
+++ src/Lecture05/Erica/SortCoordinates2.java
@@ -13,17 +13,17 @@ public class SortCoordinates2 {
 		Scanner in = new Scanner(System.in);
 
 		int number = in.nextInt();
-		ArrayList<Pair> coordinate = new ArrayList<Pair>();
+		ArrayList<Pair2> coordinate = new ArrayList<Pair2>();
 		for(int i = 0; i < number; i++)
 		{
 			int x = in.nextInt();
 			int y = in.nextInt();
-			coordinate.add(new Pair(x, y));
+			coordinate.add(new Pair2(x, y));
 		}
 		
-		Collections.sort(coordinate, new Comparator<Pair>() {
+		Collections.sort(coordinate, new Comparator<Pair2>() {
 		    @Override
-		    public int compare(final Pair o1, final Pair o2) {
+		    public int compare(final Pair2 o1, final Pair2 o2) {
 		        if(o1.getY() > o2.getY())
 		        	return 1;
 		        else if(o1.getY() == o2.getY() && o1.getX() > o2.getX())
@@ -38,11 +38,11 @@ public class SortCoordinates2 {
 	}
 }
 
-class Pair {
+class Pair2 {
 	private int x;
 	private int y;
 	
-	public Pair(int x, int y) {
+	public Pair2(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
